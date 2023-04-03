28c50229ba5a9c10d96b04a403e427d9d13d8671
David Radjai <dradjai@yahoo.com>
2023-04-03 15:52:02 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("I am index");
+})
+
+app.get('/hello', (req, res) => {
+  res.send("Hello There");
+})
+
+
+
+
+app.listen(PORT, () =>{
+  console.log(`Listening on port http://localhost:${PORT}.`);
+})
